60e8ead7f6071249cf5713b2e535ee6f80211cb6
norvey gonzalez <norveyg@gmail.com>
2018-07-12 20:53:01 -0500

    javacript

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,57 @@
 var variable=12;
 var $variable
 var _principal
-var elemento=document.createElement('h2')
-elemento.innerHTML="vinculado al codigo"
-//impresion en consola
-console.log(elemento)
 var contenedor=document.getElementsByClassName('contenedor')
-contenedor[0].appendChild(elemento)
+//impresion en consola
+//console.log(elemento)
 console.log(contenedor)
-/*for(var i;i<10;i++){
-
-}*/
 var arregloTexto = ["elem1","elem2","elem3","elem4","elem5"]
+var elementos=[]
+for(var i=0;i<arregloTexto.length;i++){
+    elementos.push(document.createElement('h2'))
+    //var elemento=document.createElement('h2')
+    elementos[i].innerHTML=arregloTexto[i]
+    if(arregloTexto[i]=='elem1'){
+
+    }else{
+    contenedor[0].appendChild(elementos[i])
+    }
+    //elemento.innerHTML="vinculado al codigo"
+    //contenedor[0].appendChild(elemento)
+}
 arregloTexto.forEach(function(interno){
     console.log(interno)
+    /*tambien de esta manera
+    elementos.push(document.createElement('h2'))
+    elementos[i].innerHTML=arregloTexto[i]
+    contenedor[0].appendChild(elementos[i])*/
 })
+/*tambien de esta manera
+for (dato in arregloTexto){
+    elementos.push(document.createElement('h2'))
+    elementos[i].innerHTML=dato
+    contenedor[0].appendChild(elementos[i])
+}*/
+
+var estado=false;
+estado=(1 == 3);
+estado = ("1" == 1);
+estado = (false || true);
+estado = (true && true);
+
+var miFuncion=function(arg1,arg2,arg3){
+    var respuesta=0;
+    respuesta=arg1+arg2+arg3;
+    return respuesta;
+}
+
+miResultado=miFuncion(10,20,30);
+console.log(miResultado);
+
+var callBack=function(numero,llamado){
+    var respuesta=numero*1000;
+    llamado(respuesta);
+}
+callBack(5,function(respuestaInterna){
+    console.log(respuestaInterna);
+});
